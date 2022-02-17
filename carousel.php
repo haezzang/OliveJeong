a1f637b23ef3950ec34c35872fd6dc72ce927759
[haezzang] <songna89@naver.com>
2022-02-17 22:24:57 +0900

    캐러셀 수정

---
+++ carousel.php
@@ -0,0 +1,72 @@
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="margin-top:40px" >
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 4"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 5"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 6"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5" aria-label="Slide 7"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="5000">
+          <img src="img/carousel/c4.jpg" class="d-block w-200" alt="..." >
+          <div class="carousel-desc">
+            <p class="desc-top">비건뷰티 런칭<br>ㅡ</p>
+            <p class="desc-mid">뮤즈 3인의<br>컬러풀한<br>비건 모먼트</p>
+            <p class="desc-bottom">나다운 가치소비를 만나보세요!</p>
+          </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000" >
+          <img src="img/carousel/c3.jpg" class="d-block w-200" alt="...">
+          <div class="carousel-desc">
+            <p class="desc-top">향기나는 욕실<br>ㅡ</p>
+            <p class="desc-mid">어디서든<br>당신에게<br>좋은 향이 나요</p>
+            <p class="desc-bottom">욕실 향 및 바디 제품 증정기획</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/carousel/c1.jpg" class="d-block w-200" alt="...">
+          <div class="carousel-desc">
+            <p class="desc-top">에스쁘아<br>ㅡ</p>
+            <p class="desc-mid">올영에서<br>먼저 만나는<br>새로운 벨벳쿠션</p>
+            <p class="desc-bottom">NEW비 벨벳 25%할인&증정</p>
+          </div>
+        </div>
+    
+        <div class="carousel-item">
+          <img src="img/carousel/c2.jpg" class="d-block w-200" alt="...">
+          <div class="carousel-descb">
+            <p class="desc-top">라포티셀<br>ㅡ</p>
+            <p class="desc-mid">판테놀 10%로<br>환절기에도<br>건조함 없이</p>
+            <p class="desc-bottom">더보이즈 포토카드 증정</p>
+          </div>
+        </div>
+    
+        <div class="carousel-item">
+          <img src="img/carousel/c5.jpg" class="d-block w-200" alt="...">
+          <div class="carousel-descb">
+            <p class="desc-top">투페이스드<br>ㅡ</p>
+            <p class="desc-mid">사랑스러운<br>하트 팁으로<br>정교하게 그려요</p>
+            <p class="desc-bottom">전상품 최대 20%할인</p>
+          </div>
+        </div>
+    
+        <div class="carousel-item">
+          <img src="img/carousel/c6.jpg" class="d-block w-200" alt="...">
+          <div class="carousel-descb">
+            <p class="desc-top">삐아<br>ㅡ</p>
+            <p class="desc-mid">늘 찾았던<br>올리브정속 삐아<br>지금 만나요</p>
+            <p class="desc-bottom">단독 선런칭 및 입점기념 30%할인</p>
+          </div>
+        </div>
+    
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <title>올리브영</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="index.css?after">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
@@ -16,7 +16,7 @@
    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
-  <style> .carousel-inner > .carousel-item > img{ /* width: 640px; height: 720px; */ } </style>
+
 
 
 
@@ -35,8 +35,11 @@
         <li class="menu1"><a href="" class="a1">매장안내</a></li>
         <li class="menu1"><a href="" class="a1">주문배송</a></li>
         <li class="menu1" ><a href=""class="a1">장바구니</a></li>
+        <li class="menu1" ><a href=""class="a1">로그인</a></li>
+        <li class="menu1" ><a href=""class="a1">회원가입</a></li>
+      </ul>
 
-<?php
+<!-- <?php
          session_start(); //세션시작
         if(!isset( $_SESSION["id"]) || !isset($_SESSION['pass']) ){
            echo  "<li class='menu1' ><a href='login/login.html' class='a1' >로그인</a></li>";
@@ -48,22 +51,24 @@
        
         <li class="menu1" ><a href="login/join.html" class="a1">회원가입</a></li>    
     </ul>
-    <br>
+    <br> -->
 
         
-   
-        
+    <br>
           <div>
             <a href="index.php"><img id="logo" src="img/logo.png"></a>
           
+            <!-- 검색 -->
             <input type="text" name="" id="search" style="padding-left : 20px;">
 
             <div class="tip_div">
+    
               <a href="login/login.html" class="tip">
-                최근 본 상품<span><br><br<br><br><br>최근 본 상품이 없습니다</span></a>
+                | &nbsp 최근 본 상품<span><br><br<br><br><br>최근 본 상품이 없습니다</span></a>
               <a href="#" class="tip" style="float: right;">
-                관심매장 소식<span><br><br>로그인하시면 자주가는 매장을<br>관심매장으로 설정 할 수있습니다<button id="tipId">로그인</button></span></a>
-           
+                | &nbsp  관심매장 소식<span><br><br>로그인하시면 자주가는 매장을<br>관심매장으로 설정 할 수있습니다<button id="tipId">로그인</button></span></a>
+                <a href="" class="tip">
+                 오늘드림<span><br><br<br><br><br>배송지를 등록하고 오늘드림으로<br>구매 가능한 상품을 확인 하세요!</span></a>
             </div>
     
           </div>
@@ -72,7 +77,7 @@
         <!-- 목록 -->    
     <hr style="margin :-10px;">
     
-    <ul class="menu2_ul" style="margin-top : 20px; margin-left : 200px;" >
+    <ul class="menu2_ul" style="margin-top :25px; margin-left: 250px; " >
         <li class="menu2"><a href="item/item1_add.php" class="a2">아이</a></li>
         <li class="menu2"><a href="#" class="a2">립</a></li>
         <li class="menu2"><a href="#" class="a2">페이스</a></li>
@@ -85,85 +90,50 @@
         <li class="menu2"><a href="#" class="a2">화장소품</a></li>
         <li class="menu2"><a href="#" class="a2">이벤트</a></li>
     </ul>
-    <br> 
+
+    <!-- 캐러셀 -->
+    <?php
+          include ("./carousel.php");
+    ?>
+   
+   
+   <!-- 배너광고 -->
+      <br><br>
+     <p style="margin-left:250px;">
+      <img src="img/banner/b1.jpg"  style="width:500px">
+      <img src="img/banner/b2.jpg" style="width:500px"></p>
+
+      <br><br>
 
 
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="margin-top : 10px;">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 4"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 5"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 6"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5" aria-label="Slide 7"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="5000">
-      <img src="img/carousel/c4.jpg" class="d-block w-150" alt="..." >
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+      <div> 
+         
+         <p style="font-weight: bold; font-size: 25px; margin-left:250px;">유사한 고객님이 <br> 많이 구매했어요</p>
+   
+            <div class="card-group"  style="width:500px; margin-left: 250px; border:none;"  >  
+            <div class="card" style="border:none; ">
+                <img src="img/best/b1.jpg" class="card-img-top" alt="...">
+                <div >
+                  <p style="font-size: 15px">마녀공장 비피다 바이옴 앰플 토너 400ml+100ml 기획 
+                  <br><a style="color: red; font-size: 15px; font-weight: bold;">17,500원</a><p>
+                </div>
+              </div>
+            <div style=" margin-right: 50px;"></div>
+              <div class="card" style="border:none;">
+                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014828505ko.jpg?l=ko" class="card-img-top" alt="...">
+                <div >
+                <p style="font-size: 15px">마녀공장 비피다 바이옴 앰플 토너 400ml+100ml 기획 
+                  <br><a style="color: red; font-size: 15px; font-weight: bold;">17,500원</a><p>
+                </div>
+              </div>      
       </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="2000" >
-      <img src="img/carousel/c2.jpg" class="d-block w-150" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="img/carousel/c1.jpg" class="d-block w-150" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-
-    <div class="carousel-item">
-      <img src="img/carousel/c3.jpg" class="d-block w-150" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-
-    <div class="carousel-item">
-      <img src="img/carousel/c5.jpg" class="d-block w-150" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-
-    <div class="carousel-item">
-      <img src="img/carousel/c6.jpg" class="d-block w-150" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-          <br><br>
+     
+      <button class="item">다른상품 추천 해드릴게요</button>
 
 
 
-          
-          <p style="padding-left: 150px;">
-            <img src="https://image.oliveyoung.co.kr/uploads/images/display/90000010001/138/462471315307755589.jpg" alt="">
-            <img src="https://image.oliveyoung.co.kr/uploads/images/display/90000010001/138/5150525810858867733.jpg" alt=""></p>
+
+
 
             <br><br><br>
             <center><p style="font-weight: bold; font-size: 30px;">Weekly Special</p></center>
@@ -176,116 +146,7 @@
 
       
 
-         
-
-           <br><br>
-            <center><p style="font-weight: bold; font-size: 30px;">인기상품만 모았어요!</p></center>
-            <div class="card-group"  style="width:60rem; margin-left: 300px;"  >
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014828505ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div >
-                  <p style="font-size: 15px">마녀공장 비피다 바이옴 앰플 토너 400ml+100ml 기획<p>
-                  <p style="color: red; font-size: 22px; font-weight: bold;">  17,500원</p>
-                </p>
-                <center><span class="badge rounded-pill bg-blue">증정</span>
-                  <span class="bg rounded-pill bg-red">세일</span>
-                  <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014861011ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div>
-                  <h6 style="font-size: 15px;">[NEW기획] 에스쁘아 프로테일러 비파우더 쿠션 기획세트 (본품+리필+선크림 20ml)</h6>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">23,200원</p>
-                </p>
-                <center><span class="badge rounded-pill bg-blue">증정</span>
-                  <span class="bg rounded-pill bg-green">쿠폰</span>
-                  <span class="bg rounded-pill bg-red">세일</span>
-                  <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/400/10/0000/0015/A00000015199104ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div >
-                  <h6 style="font-size: 15px;">닥터지 레드블레미쉬 클리어 수딩 크림 1+1 기획세트 (70ml+70ml)</h6>
-                  <br>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">  28,800원</p>
-                <center><span class="badge rounded-pill bg-blue">증정</span>
-                  <span class="bg rounded-pill bg-green">쿠폰</span>
-                  <span class="bg rounded-pill bg-red">세일</span></center>
-                </div>
-              </div>
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0015/A00000015079903ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div >
-                  <h6 style="font-size: 15px;">바비 브라운 엑스트라 립 틴트 베어 라즈베리 1+1기획</h6>
-                  <br>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">  43,000원
-                  </p>
-                  <center><span class="badge rounded-pill bg-blue">증정</span>
-                    <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-            </div>
-            
-            <br><br>
-          
-            <div class="card-group"  style="width:60rem; margin-left: 300px;"  >
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/400/10/0000/0000/A00000000688302ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div >
-                  <p style="font-size: 15px">CJ 이너비 아쿠아뱅크 56캡슐 (4주분)<p>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">  19,800원</p>
-                </p>
-                <center>
-                  <span class="bg rounded-pill bg-red">세일</span><span class="bg rounded-pill bg-green">쿠폰</span></center>
-                </div>
-              </div>
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0013/A00000013225801ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div>
-                  <h6 style="font-size: 15px;">뉴트리원라이프 루테인지아잔틴164 30캡슐 (1개월분)</h6>   <br>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">16,900원</p>
-                </p>
-                <center>
-                  <span class="bg rounded-pill bg-green">쿠폰</span>
-                  <span class="bg rounded-pill bg-red">세일</span>
-                  <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0015/A00000015038301ko.jpg?l=ko" class="card-img-top" alt="...">
-                <div >
-                  <h6 style="font-size: 15px;">니들리 데일리 토너 패드 60매 기획 (추가 5매 증정)</h6>
-                  <br>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">  18,900원</p>
-                <center>
-                  <span class="bg rounded-pill bg-green">쿠폰</span>
-                  <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-
-              <div class="card">
-                <img src="https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0015/A00000015193506ko.jpg?l=ko class="card-img-top" alt="...">
-                <div >
-                  <h6 style="font-size: 15px;">★올영 단독★ 포렌코즈 타투 올프루프 아이라이너 1+1 기획세트</h6>
-                  <br>
-                  <p style="color: red; font-size: 20px; font-weight: bold;">  14,900원
-                  </p>
-                  <center><span class="badge rounded-pill bg-blue">증정</span>
-                    <span class="bg rounded-pill bg-green">쿠폰</span>
-                    <span class="bg rounded-pill bg-red">세일</span>
-                    <span class="bg rounded-pill bg-pink">오늘드림</span></center>
-                </div>
-              </div>
-
-            </div>
-      
-         
-            <br>  <br>
-                
-             </div>
-
+          <center><p style="font-weight: bold; font-size: 25px;">BEST ITEM만 모았어요!</p></center>
      
      
             <br><br>
@@ -294,6 +155,23 @@
 
             <br><br>
             <center><p style="font-weight: bold; font-size: 25px;">주목해야 할 브랜드</p></center>
+
+
+            <table class="table table-bordered">
+  <tbody>
+    <tr>
+      <td>아벤느</td>
+      <td>아비브</td>
+      <td>투쿨포스쿨</td>
+      <td>이세이미야케</td>
+      <td>세리박스</td>
+    </tr>
+
+  </tbody>
+</table>
+
+
+
             <ul id="menu3_ul">
               <li class="menu3"><a href="menu/menu1.html" target="frame" class="a3">아벤느</a></li>
               <li class="menu3"><a href="menu/menu2.html" target="frame" class="a3">아비브</a></li>
